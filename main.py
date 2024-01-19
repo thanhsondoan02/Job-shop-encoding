191570cf79c7230895de5c9932add780407326de
thanhsondoan02 <thanhsondoan02@gmail.com>
2024-01-19 14:35:26 +0700

    propositional variable

--- main.py
+++ main.py
@@ -19,4 +19,66 @@ def printJobs(jobs):
       print("machine: ", operation[0], "time: ", operation[1])
     print("")
 
-printJobs(readFile())
+class Operation:
+  def __init__(self, jobIndex, operationIndex, machine, processingTime):
+    self.jobIndex = jobIndex
+    self.operationIndex = operationIndex
+    self.machine = machine
+    self.processingTime = processingTime
+
+class Precedes:
+  def __init__(self, jobIndex1, opeIndex1, jobIndex2, opeIndex2):
+    self.jobIndex1 = jobIndex1
+    self.opeIndex1 = opeIndex1
+    self.jobIndex2 = jobIndex2
+    self.opeIndex2 = opeIndex2
+
+  def __hash__(self):
+    return hash((self.jobIndex1, self.opeIndex1, self.jobIndex2, self.opeIndex2))
+
+  def __eq__(self, other):
+    if isinstance(other, Precedes):
+      return (self.jobIndex1 == other.jobIndex1 and self.opeIndex1 == other.opeIndex1 
+              and self.jobIndex2 == other.jobIndex2 and self.opeIndex2 == other.opeIndex2)
+    return False
+
+class StartAfter:
+  def __init__(self, jobIndex, opeIndex, time):
+    self.jobIndex = jobIndex
+    self.opeIndex = opeIndex
+    self.time = time
+
+  def __hash__(self):
+    return hash((self.jobIndex, self.opeIndex, self.time))
+
+  def __eq__(self, other):
+    if isinstance(other, StartAfter):
+      return (self.jobIndex == other.jobIndex and self.opeIndex == other.opeIndex 
+              and self.time == other.time)
+    return False
+
+class EndBefore:
+  def __init__(self, jobIndex, opeIndex, time):
+    self.jobIndex = jobIndex
+    self.opeIndex = opeIndex
+    self.time = time
+
+  def __hash__(self):
+    return hash((self.jobIndex, self.opeIndex, self.time))
+
+  def __eq__(self, other):
+    if isinstance(other, EndBefore):
+      return (self.jobIndex == other.jobIndex and self.opeIndex == other.opeIndex 
+              and self.time == other.time)
+    return False
+
+variables = {}
+count = 0
+
+def addVariable(precedes):
+  global count
+  if precedes not in variables:
+    count += 1
+    variables[precedes] = count
+  return variables[precedes]
+
