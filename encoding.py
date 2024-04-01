ca45a9f92a1a1bed7d4cb44602e09cc03cbb3b4e
thanhsondoan02 <thanhsondoan02@gmail.com>
2024-04-01 10:13:19 +0700

    split variables class to new file

--- encoding.py
+++ encoding.py
@@ -1,5 +1,5 @@
 import pygame
-
+from variables import Precedes, StartAfter, EndBefore
 
 def jobsFromFile(file_path):
   jobs = []
@@ -32,68 +32,6 @@ class Operation:
 
   def __repr__(self):
     return f"Ope({self.jobIndex},{self.operationIndex},{self.machine}, {self.processingTime})"
-
-
-class Precedes:
-  def __init__(self, jobIndex1, opeIndex1, jobIndex2, opeIndex2):
-    self.jobIndex1 = jobIndex1
-    self.opeIndex1 = opeIndex1
-    self.jobIndex2 = jobIndex2
-    self.opeIndex2 = opeIndex2
-
-  def __hash__(self):
-    return hash((self.jobIndex1, self.opeIndex1, self.jobIndex2, self.opeIndex2))
-
-  def __eq__(self, other):
-    if isinstance(other, Precedes):
-      return (self.jobIndex1 == other.jobIndex1 and self.opeIndex1 == other.opeIndex1
-              and self.jobIndex2 == other.jobIndex2 and self.opeIndex2 == other.opeIndex2)
-    return False
-
-  def __str__(self):
-    return f"Pr({self.jobIndex1},{self.opeIndex1},{self.jobIndex2},{self.opeIndex2})"
-
-  def __repr__(self):
-    return f"Pr({self.jobIndex1},{self.opeIndex1},{self.jobIndex2},{self.opeIndex2})"
-
-
-class StartAfter:
-  def __init__(self, jobIndex, opeIndex, time):
-    self.jobIndex = jobIndex
-    self.opeIndex = opeIndex
-    self.time = time
-
-  def __hash__(self):
-    return hash((self.jobIndex, self.opeIndex, self.time))
-
-  def __eq__(self, other):
-    if isinstance(other, StartAfter):
-      return (self.jobIndex == other.jobIndex and self.opeIndex == other.opeIndex
-              and self.time == other.time)
-    return False
-
-  def __repr__(self) -> str:
-    return f"Sa({self.jobIndex},{self.opeIndex},{self.time})"
-
-
-class EndBefore:
-  def __init__(self, jobIndex, opeIndex, time):
-    self.jobIndex = jobIndex
-    self.opeIndex = opeIndex
-    self.time = time
-
-  def __hash__(self):
-    return hash((self.jobIndex, self.opeIndex, self.time))
-
-  def __eq__(self, other):
-    if isinstance(other, EndBefore):
-      return (self.jobIndex == other.jobIndex and self.opeIndex == other.opeIndex
-              and self.time == other.time)
-    return False
-
-  def __repr__(self) -> str:
-    return f"Eb({self.jobIndex},{self.opeIndex},{self.time})"
-
 
 variables = {}
 count = 0
@@ -250,7 +188,7 @@ def encoding(inputPath, outputPath, variablesPath, L):
   alarm()
 
 
-for i in range(54, 57):
+for i in range(53, 58):
   outputFileName = f"ft06_L{i}_encoded.cnf"
   variableFileName = f"ft06_L{i}_variables.txt"
   print(f"Start encoding ft06 L{i}")
---
+++ variables.py
@@ -0,0 +1,59 @@
+class Precedes:
+  def __init__(self, jobIndex1, opeIndex1, jobIndex2, opeIndex2):
+    self.jobIndex1 = jobIndex1
+    self.opeIndex1 = opeIndex1
+    self.jobIndex2 = jobIndex2
+    self.opeIndex2 = opeIndex2
+
+  def __hash__(self):
+    return hash((self.jobIndex1, self.opeIndex1, self.jobIndex2, self.opeIndex2))
+
+  def __eq__(self, other):
+    if isinstance(other, Precedes):
+      return (self.jobIndex1 == other.jobIndex1 and self.opeIndex1 == other.opeIndex1
+              and self.jobIndex2 == other.jobIndex2 and self.opeIndex2 == other.opeIndex2)
+    return False
+
+  def __str__(self):
+    return f"Pr({self.jobIndex1},{self.opeIndex1},{self.jobIndex2},{self.opeIndex2})"
+
+  def __repr__(self):
+    return f"Pr({self.jobIndex1},{self.opeIndex1},{self.jobIndex2},{self.opeIndex2})"
+
+
+class StartAfter:
+  def __init__(self, jobIndex, opeIndex, time):
+    self.jobIndex = jobIndex
+    self.opeIndex = opeIndex
+    self.time = time
+
+  def __hash__(self):
+    return hash((self.jobIndex, self.opeIndex, self.time))
+
+  def __eq__(self, other):
+    if isinstance(other, StartAfter):
+      return (self.jobIndex == other.jobIndex and self.opeIndex == other.opeIndex
+              and self.time == other.time)
+    return False
+
+  def __repr__(self) -> str:
+    return f"Sa({self.jobIndex},{self.opeIndex},{self.time})"
+
+
+class EndBefore:
+  def __init__(self, jobIndex, opeIndex, time):
+    self.jobIndex = jobIndex
+    self.opeIndex = opeIndex
+    self.time = time
+
+  def __hash__(self):
+    return hash((self.jobIndex, self.opeIndex, self.time))
+
+  def __eq__(self, other):
+    if isinstance(other, EndBefore):
+      return (self.jobIndex == other.jobIndex and self.opeIndex == other.opeIndex
+              and self.time == other.time)
+    return False
+
+  def __repr__(self) -> str:
+    return f"Eb({self.jobIndex},{self.opeIndex},{self.time})"
